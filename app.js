bf0ba179f9651262472aedc55ed4255225948ead
hardWorker57 <99531629+hardWorker57@users.noreply.github.com>
2023-12-01 01:01:13 +0600

    finished-quiz

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// import { quizData } from "./data";
+// import { quizData } from "./data"; object-data
 const quizData = [
     {
         question: 'What country has the highest life expectancy?',
@@ -6,7 +6,7 @@ const quizData = [
         b: 'Astana',
         c: 'Moscow',
         d: 'Hong Kong',
-        correct: 'Hong Kong'
+        correct: 'd'
     },
     {
         question: 'Who was the Ancient Greek God of the Sun?',
@@ -14,7 +14,7 @@ const quizData = [
         b: 'Ra',
         c: 'Dionis',
         d: 'Zevs',
-        correct: 'Apollo'
+        correct: 'a'
     },
     {
         question: 'How many ghosts chase Pac-Man at the start of each game? ',
@@ -22,7 +22,7 @@ const quizData = [
         b: '4',
         c: '6',
         d: '5',
-        correct: '4'
+        correct: 'b'
     },
     {
         question: 'What game studio makes the Red Dead Redemption series?',
@@ -30,7 +30,7 @@ const quizData = [
         b: 'Rockstar Games',
         c: 'Dice',
         d: 'Electonic arts',
-        correct: 'Rockstar Games'
+        correct: 'b'
     },
     {
         question: 'What character have both Robert Downey Jr. and Benedict Cumberbatch played?',
@@ -38,39 +38,93 @@ const quizData = [
         b: 'Dracula',
         c: 'Nathan Drake',
         d: 'Sherlock Holmes',
-        correct: 'Sherlock Holmes'
+        correct: 'd'
     }
 ]
 
+let quiz = document.getElementById("quiz");
+const answerEls = document.querySelectorAll('.answer');
 let quizQuestion = document.getElementById('quiz__question');
 let a_text = document.querySelector('.quiz__answer__1');
 let b_text = document.querySelector('.quiz__answer__2');
 let c_text = document.querySelector('.quiz__answer__3');
 let d_text = document.querySelector('.quiz__answer__4');
 const submit = document.getElementById('submit');
+let images = document.querySelector('.section_image');
 
 let currentQuiz = 0;
+let score = 0;
 
 loadQuiz();
 
 function loadQuiz() {
-    const currentQuizData = quizData[currentQuiz];
+    deselectAnswers()
+    var currentQuizData = quizData[currentQuiz];
 
     quizQuestion.innerText = currentQuizData.question;
     a_text.innerText = currentQuizData.a;
     b_text.innerText = currentQuizData.b;
     c_text.innerText = currentQuizData.c;
     d_text.innerText = currentQuizData.d;
+    for (let i = 0; i < score; i++) {
+        images.innerHTML += `<img src="images/png-transparent-sasuke-uchiha-orochimaru-mitsuki-chibi-art-chibi-white-mammal-face.png" alt="">`;
+    }
 
-    // currentQuiz++;
 }
 
+function getSelected() {
+
+    let answer = undefined;
+
+    answerEls.forEach((answerEl) => {
+        if (answerEl.checked) {
+            answer = answerEl.id
+        }
+    });
+
+    return answer;
+}
+
+function deselectAnswers() {
+    answerEls.forEach((answerEl) => {
+        answerEl.checked = false;
+    });
+}
+
+
 submit.addEventListener('click', () => {
-    currentQuiz++;
-    if (currentQuiz < quizData.length) {
-        loadQuiz()
-    }
-    else {
-        alert('Thanks for accomplishing')
+    // currentQuiz++;
+
+    const answer = getSelected();
+    
+    if (answer) {
+        if (answer === quizData[currentQuiz].correct) {
+            score++
+        }
+        currentQuiz++;
+        if (currentQuiz < quizData.length) {
+            loadQuiz()
+        }
+        else {
+            quiz.innerHTML = `<h2 class="quiz_finished">Wow, you got ` + score + ` points of ` + quizData.length + `</h2>`
+        }        
     }
 })
+
+//writeText
+let textSubmit = 'Hi, my friend! This is the quiz app for getting know how good are you qualified and well-informed in common erudition';
+let index = 0;
+let animText = document.getElementById('animText')
+function writeText() {
+    animText.innerText = textSubmit.slice(0, index);
+    index++;
+   
+    if (index > textSubmit.length) {
+        index = 1
+        // animText.innerHTML = "";
+        // textSubmit.innerHTML = "";
+    }
+}
+setInterval(writeText, 40);
+
+//images for winner
